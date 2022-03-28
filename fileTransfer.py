59a656727f3b78271f0afd954f0b82a52afe2a86
Gabriel <GABEHJ2001@GMAIL.COM>
2022-03-28 14:26:35 -0700

    Update fileTransfer.py

--- fileTransfer.py
+++ fileTransfer.py
@@ -7,6 +7,8 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog as fd    #lets you use files
+import os
+import shutil
  
 
 class ParentWindow(Frame):
@@ -17,8 +19,21 @@ class ParentWindow(Frame):
 
     def getPath1(self):         #opens file manager and stores file path
         path1 = fd.askdirectory()
-        self.vartxt1.delete(0,END)
-        self.vartxt1.insert(0, path1)
+        self.txt1.delete('0','end')
+        self.txt1.insert(0, path1)
+        print(path1)
+
+    def getPath2(self):         #opens file manager and stores file path
+        path2 = fd.askdirectory()
+        self.txt2.delete('0','end')
+        self.txt2.insert(0, path2)
+
+    def copyFiles(self):
+        path1 = self.txt1.get()
+        path2 = self.txt2.get()
+        os.listdir(path1)
+        for i in path1:
+            shutil.copy(path1+i, path2)
 
     def __init__(self, master, *args, **kwargs):    #initalizes Frame
         Frame.__init__(self, master, *args, **kwargs)
@@ -29,10 +44,6 @@ class ParentWindow(Frame):
         self.master = master
         self.master.geometry('{}x{}'.format(450, 200))
         self.master.config(bg = 'lightgray')
-
-        #Variables========================================================
-        path1 = self.getPath1
-        path2 = fd.askopenfilename
 
         #blank space
         self.blank = Label(self.master, text='', bg='lightgray')
@@ -45,13 +56,13 @@ class ParentWindow(Frame):
         self.btn1.grid(row=1, column=0, padx=10, pady=10, sticky=N+W)
 
         #browse button 2
-        self.btn2 = Button(self.master, text='Browse', width=10, height=1, command=path2)
+        self.btn2 = Button(self.master, text='Browse', width=10, height=1, command=self.getPath2)
         self.btn2.grid(row=2, column=0, padx=10, pady=5, sticky=N+W)
 
-        #Check for files==================================================
+        #Copy Files=======================================================
 
         #button
-        self.btn3 = Button(self.master, text='Check for files', width=10, height=2)
+        self.btn3 = Button(self.master, text='Copy Files', width=10, height=2, command=self.copyFiles)
         self.btn3.grid(row=3, column=0, padx=3, pady=10)
 
 
@@ -65,14 +76,12 @@ class ParentWindow(Frame):
         #Textboxes========================================================
         
         #textbox 1
-        self.vartxt1 = self.getPath1
-        self.txt1 = Entry(self.master, textvariable=self.vartxt1, width=50, bg='white')
+        self.txt1 = Entry(self.master, width=50, bg='white')
         self.txt1.grid(row=1, column=1, padx=3, columnspan=3)
 
 
         #textbox 2
-        self.vartxt2 = StringVar()
-        self.txt2 = Entry(self.master, textvariable=self.vartxt2, width=50, bg='white')
+        self.txt2 = Entry(self.master, width=50, bg='white')
         self.txt2.grid(row=2, column=1, padx=3, columnspan=3)
 
 
